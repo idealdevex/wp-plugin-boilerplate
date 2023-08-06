199bae13e1b28c03e56ee2770a52a369a00f01ec
Netlogpro <netlogpro@gmail.com>
2023-08-06 17:01:35 +0330

    rename plugin main file

--- index.php
+++ index.php
@@ -12,16 +12,7 @@
 /** Block direct access to the main plugin file.*/ 
 defined( 'ABSPATH' ) || die( 'Access Denied!' );
 
-/** @var string default plugin slug holder */
-define( "IDEALDEVEX_WEB_PORTAL_SLUG" , "idealdevex-web-portal" );
-
-/** @var string plugin Version */
-define( "IDEALDEVEX_WEB_PORTAL_VERSION" , "0.1.0" );
-
-/** @var string plugin root path */
-define( "IDEALDEVEX_WEB_PORTAL_PLUGIN_FILE" , __FILE__ );
-
-require_once dirname( IDEALDEVEX_WEB_PORTAL_PLUGIN_FILE ) . "/vendor/autoload.php";
+require_once dirname( __FILE__ ) . "/vendor/autoload.php";
 
 if ( Idealdevex\Webportal\Core\App::class ) {
     \Idealdevex\Webportal\Core\App::getInstance();
